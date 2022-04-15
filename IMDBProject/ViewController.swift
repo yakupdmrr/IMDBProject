9a128d773319d3fae16f5ec8697c52169b92510c
yakupdmrr <yakupdmrr@hotmail.com>
2022-04-15 19:34:32 +0300

    Initial Commit

---
+++ IMDBProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDBProject
+//
+//  Created by Yakup Demir on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IMDBProjectUITests/IMDBProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IMDBProjectUITestsLaunchTests.swift
+//  IMDBProjectUITests
+//
+//  Created by Yakup Demir on 15.04.2022.
+//
+
+import XCTest
+
+class IMDBProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
